141e5e52277269cfbd66bbe876432719bd3f997b
Lingfeng555 <lingfeng.chen@opendeusto.es>
2025-04-26 17:31:35 +0200

    minorchanges

--- Back/blueprints/app.py
+++ Back/blueprints/app.py
@@ -1,6 +1,8 @@
 # app.py
 from flask import Flask, request, jsonify, render_template
 from flask import Blueprint, request, jsonify
+from datetime import datetime
+import psycopg2
 
 app = Flask(__name__)  # crea la aplicación
 
@@ -12,14 +14,15 @@ api_bp = Blueprint('api', __name__, url_prefix='/api/greenlake-eval')
 
 @api_bp.route('/test', methods=['GET'])
 def test1():
-    nombre = request.args.get('nombre', 'mundo')
+    timestamp = datetime.now().isoformat() + "Z"
+    status = "running"
     return jsonify({
         "metadata": {
             "status": "success",
-            "timestamp": "YYYY-MM-DDTHH:MM:SSZ"
+            "timestamp": timestamp
         },
         "results": {
-            "status": "active"
+            "status": status
         }
     })
 
