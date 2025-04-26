caa868989f0e540d705af54176256ca2a57054d0
Martin <martin.f@opendeusto.es>
2025-04-26 21:29:56 +0200

    tome señor david

--- Back/API/app.py
+++ Back/API/app.py
@@ -4,7 +4,7 @@ from flask import Blueprint, request, jsonify
 from datetime import datetime
 from blueprints.dao import cur
 import psycopg2
-from blueprints.kafka import kafka_bp, init_kafka_consumer
+from blueprints.kafka import kafka_bp
 from blueprints.api_greenlake_br import api_bp
 from flask_cors import CORS
 
@@ -20,7 +20,5 @@ app.register_blueprint(api_bp)
 app.register_blueprint(kafka_bp)
 
 if __name__ == '__main__':
-    # Initialize Kafka consumer if needed
-    init_kafka_consumer()
     # Use standard Flask run method instead of socketio.run
     app.run(debug=True, host='0.0.0.0', port=5454)
