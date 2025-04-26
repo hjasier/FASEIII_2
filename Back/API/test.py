5fce3cee53d77e73337b5d6631abb4e913c388cf
Lingfeng555 <lingfeng.chen@opendeusto.es>
2025-04-26 17:09:42 +0200

    nada

--- Back/API/test.py
+++ Back/API/test.py
@@ -8,9 +8,39 @@ def index():
     return render_template('index.html')  # devuelve una plantilla
 
 @app.route('/api/greenlake-eval/test', methods=['GET'])
-def saludo():
+def test1():
     nombre = request.args.get('nombre', 'mundo')
-    return jsonify({ 'mensaje': f'¡Hola, {nombre}!' })
+    return jsonify({
+            "metadata": {
+                "status": "success",
+                "timestamp": "YYYY-MM-DDTHH:MM:SSZ"
+            },
+            "results": {
+                "status": "active"
+            }
+        })
+    
+@app.route('/api/greenlake-eval/hospitals/nearby', methods=['GET'])
+def test2():
+    lat = request.args.get('lat', '79.8965515')
+    lon = request.args.get('lon', '-48.0003246')
+    radius = request.args.get('radius', '1000')
+    return jsonify({
+        "metadata": {
+            "status": "success",
+            "timestamp": "YYYY-MM-DDTHH:MM:SSZ"
+        },
+        "results": [
+            {
+                "id": "86cbe6d4-f169-4867-8672-8ec4609b6293",
+                "city_id": "371197f0-1599-4e3a-a0a7-c90da11bc5b6",
+                "name": "Hielo Alto Medical Center",
+                "longitude": float(lon),
+                "latitude": float(lat),
+                "distance_m": 0.0
+            }
+        ]
+    })
     
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5656)
+    app.run(debug=True, host='0.0.0.0', port=5454)
