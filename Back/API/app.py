bab4088cf29b371dbf8088d59b7cf4b620086389
Lingfeng555 <lingfeng.chen@opendeusto.es>
2025-04-26 17:39:26 +0200

    asdsads

--- Back/API/app.py
+++ Back/API/app.py
@@ -2,6 +2,7 @@
 from flask import Flask, request, jsonify, render_template
 from flask import Blueprint, request, jsonify
 from datetime import datetime
+from dao import cur
 import psycopg2
 
 app = Flask(__name__)  # crea la aplicación
@@ -48,6 +49,31 @@ def test2():
         ]
     })
 
+@api_bp.route('/events/nearby', methods=['GET'])
+def events_nearby():
+    # Parámetros requeridos
+    city_id   = request.args.get('city_id')
+    start_str = request.args.get('start_date')
+    end_str   = request.args.get('end_date')
+    
+    return jsonify({
+        "metadata": {
+            "status": "success",
+            "timestamp": "YYYY-MM-DDTHH:MM:SSZ"
+        },
+        "results": [
+            {
+                "city_id": "3aedcf9c-e38d-4377-bade-6e27dd7c9b22",
+                "description": "Join us for an unforgettable night of Punk music with Crystal Farrell",
+                "end_date": "Sat, 11 Jan 2025 16:37:21 GMT",
+                "event_id": "2d9c9a38-8cb9-4226-813c-9be3ce1c66d3",
+                "name": "Kylie Black Concert",
+                "start_date": "Wed, 08 Jan 2025 16:37:21 GMT",
+                "venue_id": "d9075054-085b-4c89-ad7b-19e95b5b5cd1"
+            }
+        ]
+        })
+    
 app.register_blueprint(api_bp)
     
 if __name__ == '__main__':
